32933660c2f88d63b6f711ac28eee1dadd2597ff
Matthieudegny <matthieu.dev.degny@gmail.com>
2023-05-07 11:03:30 +0200

    page creation

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import React, { ReactNode } from "react";
+import "@/styles/globals.css";
+
+// import Nav from "@components/Nav";
+// import Provider from "@components/Provider";
+
+interface Metadata {
+  title: string;
+  description: string;
+}
+
+export const metadata: Metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
+
+interface RootLayoutProps {
+  children: ReactNode;
+}
+
+const RootLayout = ({ children }: RootLayoutProps): JSX.Element => (
+  <html lang="en">
+    <body>
+      <Provider>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
+
+export default RootLayout;
