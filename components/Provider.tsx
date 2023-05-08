42c0b4fe87e268d2cc97da42bd6b8624cecceaae
Matthieudegny <matthieu.dev.degny@gmail.com>
2023-05-08 11:24:25 +0200

    auth process

---
+++ components/Provider.tsx
@@ -0,0 +1,13 @@
+import { SessionProvider } from "next-auth/react";
+import { Session } from "next-auth";
+
+interface ProviderProps {
+  session: Session;
+  children: React.ReactNode;
+}
+
+const Provider: React.FC<ProviderProps> = ({ children, session }) => (
+  <SessionProvider session={session}>{children}</SessionProvider>
+);
+
+export default Provider;
