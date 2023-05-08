fd6f79e6ad8cce42f6506bccb388e8ad26454580
Matthieudegny <matthieu.dev.degny@gmail.com>
2023-05-08 14:10:18 +0200

    error in id mongodb env

--- components/Provider.tsx
+++ components/Provider.tsx
@@ -3,7 +3,7 @@ import { SessionProvider } from "next-auth/react";
 import { Session } from "next-auth";
 
 interface ProviderProps {
-  session: Session;
+  session?: Session;
   children: React.ReactNode;
 }
 
